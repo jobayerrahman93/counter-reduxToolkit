0f04bc3b78f66b24e0b6140989d1ade7b2060c92
jobayercse71 <jobayercse71.com>
2022-07-13 23:52:46 +0600

    provider add

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from './slice/counterSlice'
-const store = configureStore({
+export const store = configureStore({
     reducer:{
         counter:counterSlice
     }
